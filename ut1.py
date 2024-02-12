8d60f4781f5734771de0adea8144f2eb0ca67ae8
LegionArc <omsakhare023@gmail.com>
2024-02-12 23:25:59 +0530

    Practical 4

---
+++ ut1.py
@@ -0,0 +1,28 @@
+""" t1 = [4, 7, 2, 9, 6]
+print(t1[2])
+print(t1[-1])
+print(t1[2:])
+print(t1[2:4])
+"""
+"""
+ li2 = ['java', 'python' , 'php' ]
+li2.append("VB")
+li2.append("java")
+print(li2)
+li2.remove("php")
+print(li2)
+li2.reverse()
+print(li2)
+print(li2.count("java"))
+print(min(li2))
+print(max(li2))"""
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+
+li3 = [1,2,18,209]
+print(min(li3))
+print(max(li3))
+
