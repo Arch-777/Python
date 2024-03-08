3775201ee40b79771d6137f5672881635095564e
LegionArc <omsakhare023@gmail.com>
2024-03-08 16:25:20 +0530

    Practical 7

---
+++ Exp7_1.py
@@ -0,0 +1,3 @@
+t1 = (12, 23, 34, 45, 56)
+print(min(t1))
+print(max(t1))
---
+++ Exp7_3.py
@@ -0,0 +1,5 @@
+t1 = ('zero', 'one', 'two', 'three', 'four', 'five')
+n = input("Enter a num ")
+for i in n:
+    c = int(i)
+    print(t1[c], end=" ")
